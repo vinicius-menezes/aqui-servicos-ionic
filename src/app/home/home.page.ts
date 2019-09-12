cf6c9a20cc395012146c13679ff43680932ea3f4
Vinicius <vicmen22@gmail.com>
2019-09-12 11:01:57 -0400

    Abrindo site no navegador e internamente

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {InAppBrowser, InAppBrowserOptions} from '@ionic-native/in-app-browser/ngx';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
+    options: InAppBrowserOptions = {
+        location: 'yes',//Or 'no'
+        hidden: 'no', //Or  'yes'
+        clearcache: 'yes',
+        clearsessioncache: 'yes',
+        zoom: 'yes',//Android only ,shows browser zoom controls
+        hardwareback: 'yes',
+        mediaPlaybackRequiresUserAction: 'no',
+        shouldPauseOnSuspend: 'no', //Android only
+        closebuttoncaption: 'Close', //iOS only
+        disallowoverscroll: 'no', //iOS only
+        toolbar: 'yes', //iOS only
+        enableViewportScale: 'no', //iOS only
+        allowInlineMediaPlayback: 'no',//iOS only
+        presentationstyle: 'pagesheet',//iOS only
+        fullscreen: 'yes',//Windows only
+    };
 
+    constructor(private theInAppBrowser: InAppBrowser) {
+    }
+
+    public openWithSystemBrowser(url: string) {
+        let target = '_system';
+        this.theInAppBrowser.create(url, target, this.options);
+    }
+
+    public openWithInAppBrowser(url: string) {
+        let target = '_blank';
+        this.theInAppBrowser.create(url, target, this.options);
+    }
+
+    public openWithCordovaBrowser(url: string) {
+        let target = '_self';
+        this.theInAppBrowser.create(url, target, this.options);
+    }
 }
